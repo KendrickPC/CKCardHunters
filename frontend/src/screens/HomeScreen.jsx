8a6ccbb2cefd0db4a4289fd53acf23352ef8d99f
kendrickPC <kennethpchang@gmail.com>
2023-04-12 09:14:55 +0800

    product images showing up

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import products from '../../products'
 import { Col, Row } from 'react-bootstrap'
+import Product from '../components/Product'
 
 const HomeScreen = () => {
   return (
@@ -8,8 +9,8 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
       <Row>
         {products.map( (product) => (
-          <Col>
-            <h5>{product.name}</h5>
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
