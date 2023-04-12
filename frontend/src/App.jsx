c70c18120627cee3e5bd1909c45083903c094efb
kendrickPC <kennethpchang@gmail.com>
2023-04-12 11:06:20 +0800

    Links don't refresh the page anymore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route,  } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main className='py-5'>
       <Container>
-        <HomeScreen />
+        <Routes>
+          <Route path='/' element={<HomeScreen/>} exact />
+          <Route path='/product/:id' element={<ProductScreen/>} />
+        </Routes>
       </Container>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
