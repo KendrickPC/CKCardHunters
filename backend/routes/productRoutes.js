c7f81b2b8d8d4281aa94053074b3fc90f9c95d99
kendrickPC <kennethpchang@gmail.com>
2023-04-14 12:29:01 +0800

    Express Async Error Handler works, but not for cast errors on MongoDB servers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,18 +1,26 @@
 import express from 'express';
-import expressAsyncHandler from 'express-async-handler';
+import asyncHandler from "express-async-handler"
 const router = express.Router();
 import Product from '../models/productModel.js'
 
-// localhost:5174/api/products
-router.get('/', expressAsyncHandler(async (req, res) => {
+// @desc fetch all products
+// @route GET /api/products
+// @access PUBLIC
+router.get('/', asyncHandler(async (req, res) => {
   const product = await Product.find({})
   res.json(product)
 }))
 
-// localhost:5174/api/products/:id
-router.get('/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
+// @desc fetch single product
+// @route GET /api/products/:id
+// @access PUBLIC
+router.get('/:id', asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product)  
+  } else {
+    res.status(404).json({message: "Product Not Found"})
+  }
+}))
 
 export default router;
