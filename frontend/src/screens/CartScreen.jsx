668a1031b9463fb65ef7806e74ec06bc0f3d1d83
kendrickPC <kennethpchang@gmail.com>
2023-04-16 05:06:22 +0800

    useParams() and queryParams values obtained

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {Link, useParams} from 'react-router-dom'
+import { useDispatch, useSelector } from 'react'
+import {Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap'
+import Message from '../components/Message'
+import { addToCart } from '../actions/cartActions'
 
+// Step 1 to get the product id
 const CartScreen = () => {
+  const {id} = useParams()
+  const queryParams = new URLSearchParams(window.location.search);
+  const qty = queryParams.get('qty');
+
+  console.log('id', id);
+  console.log('qty', qty);
+
   return (
     <div>CartScreen</div>
   )
