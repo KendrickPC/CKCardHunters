54a34f75fb4b4afbc4f48063c0947fca53e0a63f
kendrickPC <kennethpchang@gmail.com>
2023-04-16 05:26:10 +0800

    Add to cart functionality completed

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,18 +1,25 @@
 import React, {useEffect} from 'react'
 import {Link, useParams} from 'react-router-dom'
-import { useDispatch, useSelector } from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import {Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap'
 import Message from '../components/Message'
 import { addToCart } from '../actions/cartActions'
 
-// Step 1 to get the product id
 const CartScreen = () => {
   const {id} = useParams()
   const queryParams = new URLSearchParams(window.location.search);
-  const qty = queryParams.get('qty');
+  const qty = Number(queryParams.get('qty'));
 
-  console.log('id', id);
-  console.log('qty', qty);
+  const dispatch = useDispatch()
+  useEffect( () => {
+    if (id) {
+      dispatch(addToCart(id, qty))
+    }
+  }, [dispatch, id, qty])
+
+  const cart = useSelector(state => state.cart)
+  const {cartItems} = cart;
+  // console.log(cartItems);
 
   return (
     <div>CartScreen</div>
