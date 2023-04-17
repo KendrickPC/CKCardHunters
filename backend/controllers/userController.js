3a90dc93cbc2a2cfe1fbd6178f9e25f7c468f199
kendrickPC <kennethpchang@gmail.com>
2023-04-17 14:26:57 +0800

    User authentication endpoint created. POSTMAN confirmed working with bcrypt compare

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,22 @@ import User from '../models/userModel.js'
 const authUser = asyncHandler(async (req, res) => {
   // Get data from the body
   const {email, password} = req.body
-  res.send({email, password})
+  // Testing for POSTMAN
+  // res.send({email, password})
+  const user = await User.findOne({email})
+  // Checking is user exists
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid email or password')
+  }
 })
 
 export {
