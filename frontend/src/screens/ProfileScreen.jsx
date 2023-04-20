fbbb98417c72f9e333646cc69d8096aa86909e5c
kendrickPC <kennethpchang@gmail.com>
2023-04-20 11:50:15 +0800

    Profile screen populates with name and email for editing profile, but no functionality yet. userDetailsReducer bug fixed

---
+++ frontend/src/screens/ProfileScreen.jsx
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+
+import { getUserDetails } from "../actions/userActions";
+
+const ProfileScreen = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState(null);
+
+  const location = useLocation();
+  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  
+  const userDetails = useSelector((state) => state.userDetails);
+  const { loading, error, user } = userDetails;
+  
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    if (!userInfo) {
+      navigate('/login')
+    } else {
+      if (!user.name) {
+        dispatch(getUserDetails('profile'))
+      } else {
+        setName(user.name)
+        setEmail(user.email)
+      }
+    }
+  }, [dispatch, navigate, userInfo, user]);
+
+  const submitHandler = (evt) => {
+    evt.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage('Passwords do not match')
+    } else {
+      // dispatch update profile
+
+    }
+    
+  };
+
+  return (
+    <Row>
+      <Col md={3}>
+      <h2>User Profile</h2>
+      {message && <Message variant="danger">{message}</Message>}
+      {error && <Message variant="danger">{error}</Message>}
+      {/* Loading Problem */}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        {/* name */}
+      <Form.Group controlId="name">
+          <Form.Label>Your name</Form.Label>
+          <Form.Control
+            type="name"
+            placeholder="Your name"
+            value={name}
+            onChange={(evt) => setName(evt.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        {/* Email */}
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Your Email"
+            value={email}
+            onChange={(evt) => setEmail(evt.target.value.toLowerCase())}
+          ></Form.Control>
+        </Form.Group>
+        {/* Password */}
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Your password"
+            value={password}
+            onChange={(evt) => setPassword(evt.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        {/* Confirm Password */}
+        <Form.Group controlId="confirmPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Confirm password"
+            value={confirmPassword}
+            onChange={(evt) => setConfirmPassword(evt.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button type="submit" variant="primary">
+          Update
+        </Button>
+      </Form>
+
+      </Col>
+      <Col md={9}>
+        <h2>My Orders</h2>
+      </Col>
+    </Row>
+  );
+};
+
+export default ProfileScreen;
